f11856522d3af7a513e17822a0daf631780f3f6a
Ssyndrome <915448715@qq.com>
2018-09-24 13:10:49 +0800

    [Syndrome][#999] initialize the project

---
+++ src/main/java/com/thoughtworks/serverproject/ServerProjectApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.serverproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerProjectApplication.class, args);
+	}
+}
